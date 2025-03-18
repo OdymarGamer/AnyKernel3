2fa3a05b6902dfc4ea93a852482764f17f6b4632
OdymarGamer <103310250+OdymarGamer@users.noreply.github.com>
2025-03-18 15:57:31 +0200

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=Wild Plus Kernel by TheWildJames or Morgan Weedman
+kernel.string=Ody Plus Kernel By OdyMarGamer
 do.devicecheck=0
 do.modules=0
 do.systemless=0
